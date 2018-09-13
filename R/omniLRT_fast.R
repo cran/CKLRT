4cde956d3748430b81302a9a67f540df4117f65d
Haoyu Zhang <andrew.haoyu@gmail.com>
2018-09-13 20:40:02 +0000

    version 0.2.3

--- R/omniLRT_fast.R
+++ R/omniLRT_fast.R
@@ -1,5 +1,6 @@
-#' Composite kernel machine regression based likelihood ratio test
+#' Composite kernel machine regression based likelihood ratio test.
 #'
+#' Composite kernel machine regression based likelihood ratio test. The approximate method for likelihood ratio test tend to be too conservative for small alpha values. We recommend not using it in GWAS
 #' @param y : y is the vecgtor of the continous outcomes.
 #' @param X : X denotes the additional covariates.
 #' @param K1 : K1 is the first kernel corresponding to the genetic main effect.
@@ -44,9 +45,9 @@ omniLRT_fast = function(y, X,K1, K2, N = 10000,length.lambda = 200, length.rho =
     px = ncol(X1)
   }
   XX   = MatMult_C(t(X1),X1)
-
+  
   P0   = diag(n)- MatMult_C(MatMult_C(X1,ginv(XX)),t(X1))
-
+  
   eP   = Eigen_C(P0)
   A    = eP$vector[,eP$values > 1e-10]
   # invP = ginv(P0)
@@ -59,34 +60,34 @@ omniLRT_fast = function(y, X,K1, K2, N = 10000,length.lambda = 200, length.rho =
   }else{
     phi1 = t(t(eK1$vectors[,wK1])*sqrt(eK1$values[wK1]))
   }
-
+  
   eK2  = Eigen_C(K2)
   wK2  = which(eK2$values  > 1e-10)
-
+  
   if (length(wK2) == 1){
     phi2 = eK2$vectors[,wK2] * sqrt(eK2$values[wK2])
   }else{
-
+    
     phi2 = t(t(eK2$vectors[,wK2])*sqrt(eK2$values[wK2]))
   }
-
+  
   group= rep(1,n)
-if(is.null(X)){
-  fit1 = lme(y~1, random = list(group=pdIdent(~-1+phi1), group = pdIdent(~-1+phi2)), method = "ML") #  Default = REML
-  fit0 = stats::lm(y~1)
-  LR = max(0, 2*(stats::logLik(fit1, REML = F) -stats::logLik(fit0, REML = F)))
-
+  if(is.null(X)){
+    fit1 = lme(y~1, random = list(group=pdIdent(~-1+phi1), group = pdIdent(~-1+phi2)), method = "ML") #  Default = REML
+    fit0 = stats::lm(y~1)
+    LR = max(0, 2*(stats::logLik(fit1, REML = F) -stats::logLik(fit0, REML = F)))
+    
   }else{
-  fit1 = lme(y~X, random = list(group=pdIdent(~-1+phi1), group = pdIdent(~-1+phi2)), method = "ML") #  Default = REML
-  fit0 = stats::lm(y~X)
-  LR = max(0, 2*(stats::logLik(fit1, REML = F) -stats::logLik(fit0, REML = F)))
-
-}
-
+    fit1 = lme(y~X, random = list(group=pdIdent(~-1+phi1), group = pdIdent(~-1+phi2)), method = "ML") #  Default = REML
+    fit0 = stats::lm(y~X)
+    LR = max(0, 2*(stats::logLik(fit1, REML = F) -stats::logLik(fit0, REML = F)))
+    
+  }
+  
   if (LR <= 0){
     p.dir = 1;
     p.au1=1; p.aud = 1
-
+    
   }else{
     #For the first kernel
     LR0_allRho = matrix(NA, N, length.rho)
@@ -98,10 +99,10 @@ if(is.null(X)){
     K = K2
     k = length(wK2)
     xi = eK2$values[wK2]
-
+    
     mu = Eigen_C_value(t(phi2) %*% P0%*% phi2)
     # mu = eigen(P0 %*% K2)$values
-
+    
     mu = mu/max(mu, xi)
     xi = xi/max(mu,xi)
     AKA = MatMult_C(MatMult_C(t(A),K),A)
@@ -110,32 +111,32 @@ if(is.null(X)){
     w.double = w^2
     w1 = (w.double)[1:k,]
     w2 = ColSum_C((w.double)[-(1:k),])
-
+    
     if (length(mu) < k){mu = c(mu,rep(0, k - length(mu)))}
     if (length(xi) < k){xi = c(xi,rep(0, k - length(xi)))}
-
+    
     LR0_fixRho <- LR0_fixRho_LRT_C(Lambdas,
-                               mu,
-                               w1,
-                               w2,
-                               n-px,
-                               xi)
+                                   mu,
+                                   w1,
+                                   w2,
+                                   n-px,
+                                   xi)
     LR0_allRho[,1] = MatrixRowMax_C(LR0_fixRho)
     LR0_allRho <- doubleloop_LRT(K1,
-                             K2,
-                             P0,
-                             A,
-                             U_1,
-                             w,
-                             Lambdas,
-                             n-px,
-                             all_rho,
-                             LR0_allRho)
+                                 K2,
+                                 P0,
+                                 A,
+                                 U_1,
+                                 w,
+                                 Lambdas,
+                                 n-px,
+                                 all_rho,
+                                 LR0_allRho)
     LR0 = MatrixRowMax_C(LR0_allRho)
     LR0 = ifelse(LR0 > 0, LR0, 0)
     p.dir = mean(LR < LR0)
     p.aud= getp_aud_estimate_pi_first(null = LR0, LR = LR)$p
-
+    
   }
   out = list(p.dir = p.dir,p.aud = p.aud, LR = LR)
   return(out)
